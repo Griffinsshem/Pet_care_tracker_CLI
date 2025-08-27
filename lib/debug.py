77c92bbd9481eb5faababf39d0099d6c8ce868a6
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-08-27 12:56:45 +0300

    feat: update the debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# lib/debug.py
-
-#create tables
+#create database tables
 
 from lib.database import Base, engine
 from lib.models import pet, care_type, care_event
 
-if __name__ == "__main__":
-    print("Creating tables...")
-    Base.metadata.create_all(engine)
-    print("Tables created successfully!")
+
+print("Creating tables...")
+Base.metadata.create_all(engine)
+print("Tables created successfully!")
